de54c0bbf2c1c1a96307399c7c2aee5250263f6d
kristofferkarlsson93 <kristoffer.karlsson93@hotmail.com>
2018-10-10 22:08:11 +0200

    stubbing cache and dependency finder to get forward

--- test/DependencyLoader/DependencyLoader.tests.js
+++ test/DependencyLoader/DependencyLoader.tests.js
@@ -8,31 +8,78 @@ const realFunctionReflector = require('js-function-reflector');
 module.exports = testCase('DependencyLoader', {
     'When given a function with no dependencies': {
         setUp() {
-            this.DependencyLoader = require('../../DependencyLoader/DependencyLoader.js')({ filesystemFacade: () => {} });
-            this.exampleFunction = sinon.stub();
+            this.dependencyCache = {
+                add: sinon.stub(),
+                get: () => null
+            };
+            const DependencyLoader = require('../../DependencyLoader/DependencyLoader.js');
+            const dependencyLoader = DependencyLoader({ dependencyCache: this.dependencyCache});
+            this.exampleFunction = sinon.stub().returns({});
 
-            this.DependencyLoader.newInstanceWithName('exampleFunction', this.exampleFunction);
+            dependencyLoader.newInstanceWithName('exampleFunction', this.exampleFunction);
         },
         'should run function': function () {
             assert.calledOnce(this.exampleFunction);
+        },
+        'should cache the instance': function () {
+            assert.calledOnce(this.dependencyCache.add);
+            assert.calledWith(this.dependencyCache.add, "exampleFunction", {});
+        },
+    },
+    'when given the same function twice': {
+        setUp() {
+            const DependencyLoader = require('../../DependencyLoader/DependencyLoader.js');
+            this.exampleFunction = sinon.stub();
+            this.dependencyCache = {
+                add: sinon.stub(),
+                get: sinon.stub()
+                    .onCall(0).returns(null)
+                    .onCall(1).returns(this.exampleFunction)
+            };
+            this.dependencyLoader = DependencyLoader({
+                dependencyCache: this.dependencyCache,
+                dependencyFinder: () => {},
+                functionReflector: realFunctionReflector
+            });
+
+            this.dependencyLoader.newInstanceWithName('exampleFunction', this.exampleFunction);
+            this.dependencyLoader.newInstanceWithName('exampleFunction', this.exampleFunction);
+        },
+        'should get cached intstance for secound call': function () {
+            assert.calledWith(this.dependencyCache.get, 'exampleFunction');
+        },
+        'should instantiate the function only once': function () {
+            assert.calledOnce(this.exampleFunction);
+        },
+    },
+    'when given a function with one dependency': {
+        setUp() {
+            const DependencyLoader = require('../../DependencyLoader/DependencyLoader.js');
+            this.exampleDependency = sinon.stub().returns(() => {});
+            const dependencyFinder = {
+                findByName: () => ({ exampleDependency: this.exampleDependency })
+            };
+            this.dependencyCache = {
+                add: sinon.stub(),
+                get: () => null
+            };
+            this.exampleModule = function ({ exampleDependency }) {return {};};
+
+            const dependencyLoader = DependencyLoader({
+                dependencyCache: this.dependencyCache,
+                dependencyFinder,
+                functionReflector: realFunctionReflector
+            });
+            dependencyLoader.newInstanceWithName('exampleModule', this.exampleModule);
+        },
+        'should run dependency': function () {
+            assert.calledOnce(this.exampleDependency);
+        },
+        'should cache dependency': function () {
+            assert.calledWith(this.dependencyCache.add, sinon.match("exampleDependency", {}));
+        },
+        'should cache function': function () {
+            assert.calledWith(this.dependencyCache.add, sinon.match("exampleModule", {}));
         }
-    },
-    'when given the same function twice should return same instance': function () {
-        const dependencyLoader = require('../../DependencyLoader/DependencyLoader.js')({ filesystemFacade: () => {} });
-        const exampleFunction = sinon.stub();
-
-        dependencyLoader.newInstanceWithName('exampleFunction', exampleFunction);
-        dependencyLoader.newInstanceWithName('exampleFunction', function () {});
-        assert.calledOnce(exampleFunction);
-    },
-    '=>when given a function with one dependency should run dependency': function () {
-        const fileSystemFacade = () => {};
-        const dependencyLoader = require('../../DependencyLoader/DependencyLoader.js')({ fileSystemFacade, functionReflector: realFunctionReflector });
-        const exampleDependency = sinon.stub();
-        const exampleModule = function ({ exampleDependency }) {};
-
-        dependencyLoader.newInstanceWithName('exampleModule', exampleModule);
-
-        assert.calledOnce(exampleDependency);
     }
 });
