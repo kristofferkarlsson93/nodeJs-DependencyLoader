156c6a9bec2c69d2fbfed162d0359e53723af770
kristofferkarlsson93 <kristoffer.karlsson93@hotmail.com>
2018-10-17 23:13:54 +0200

    Now tests that one dependency on one level can be found. Prepares for further testing

--- DependencyLoader/DependencyFinder.js
+++ DependencyLoader/DependencyFinder.js
@@ -1,41 +1,35 @@
 const path_module = require('path');
 
-module.exports = function ({fs, basePath, moduleLoader}) {
+module.exports = function ({ fs, basePath, moduleLoader }) {
     return {
         findFromArray
     };
 
-    function findFromArray (array) {
-        console.log(array);
+    function findFromArray(array) {
         const loaded = {};
         const search = function (path) {
-            fs.lstat(path, function(err, stat) {
-                // DO NOT LOG!!! STUBBED
+            fs.lstat(path, function (err, stat) {
                 if (stat.isDirectory()) {
-                    console.log('MEEE');
-                    // we have a directory: do a tree walk
-                    fs.readdir(path, function(err, files) {
-                        var f, l = files.length;
-                        for (var i = 0; i < l; i++) {
-                            f = path_module.join(path, files[i]);
-                            search(f, array);
-                        }
+                    fs.readdir(path, function (err, files) {
+                        files.forEach(file => {
+                            filePath = path_module.join(path, file);
+                            search(filePath)
+                        });
                     });
                 } else {
                     const fileName = getFileNameFromPath(path);
-                    loaded[array[0]] = loaded[array[0]];
+                    if (array.includes(fileName)) {
+                        loaded[array[0]] = moduleLoader.load(path);
+                    }
                 }
             });
-        }
+        };
         search(basePath);
         return loaded;
     }
 
     function getFileNameFromPath(path) {
-        const parts = path.split('/');
-
-        const  filename = parts[parts.length-1].replace('.js', '');
-        return filename;
+        return path.replace(/^.*[\\\/]/, '').replace('.js', '');
     }
 
 };
--- test/DependencyLoader/DependencyFinder.tests.js
+++ test/DependencyLoader/DependencyFinder.tests.js
@@ -5,15 +5,13 @@ const assert = bocha.assert;
 const refute = bocha.refute;
 
 module.exports = testCase('DependencyFinder', {
-    '=>when given an array with one requested dependency which is on the base level should return that dependency': function () {
+    'when given an array with one requested dependency which is on the base level should return that dependency': function () {
         const fakePath = 'Project/app/src';
         const fakeModuleName = 'fakeModule';
         const fakeFs = {
             lstat: function (path, callback) {
                 callback(null, {
-                    isDirectory: sinon.stub()
-                        .onCall(0).returns(true)
-                        .returns(false)
+                    isDirectory: () => !path.includes('.js')
                 });
             },
             readdir: function (path, callback) {
@@ -22,20 +20,22 @@ module.exports = testCase('DependencyFinder', {
         };
         const moduleLoader = function () {
             return {
-                load: sinon.stub()
-                    .withArgs('Project/app/src/fakeModule.js').returns({ test: 'test' })
-                    .returns()
+                load: sinon.stub().returns({ test: 'test' })
             }
         }();
         const DependencyFinder = require('../../DependencyLoader/DependencyFinder.js');
         const dependencyFinder = DependencyFinder({ basePath: fakePath, fs: fakeFs, moduleLoader });
 
         const dependencies = dependencyFinder.findFromArray([fakeModuleName]);
-        console.log('deps', dependencies);
+
         assert.equals(dependencies, { 'fakeModule': { test: 'test' } });
         assert.calledOnce(moduleLoader.load);
-        assert.calledWith(moduleLoader.load, "Project/app/src/fakeModule.js")
-    }
+        assert.calledWith(moduleLoader.load, "Project\\app\\src\\fakeModule.js")
+    },
+    '//when given an array with one dependency name should NOT care about casing': function () {},
+    '//when given an array with one dependency name but wanted file is called index.js should use the dir name as key': function () {},
+    '//when given an array with two dependency names on same level should load both': function () {},
+    '//when given an array with two dependency names on different levels should load both': function () {}
 });
 
 // TODO: implementera module loader.
